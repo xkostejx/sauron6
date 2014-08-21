fb6a11d6023edc5a2fd6939f4100e2ed07f5d8b7
Michal Kostenec <kostenec@civ.zcu.cz>
2014-08-21 12:16:42 +0200

    import: minor import fix

--- Sauron/UtilZone.pm
+++ Sauron/UtilZone.pm
@@ -191,7 +191,7 @@ sub process_zonefile($$$$) {
     elsif ($type eq 'AAAA') {
       fatal("$filename($.): invalid AAAA record: $fline")
 	unless (ip_is_ipv6($_));
-      push @{$rec->{AAAA}}, $_;
+      push @{$rec->{AAAA}}, lc($_);
     }
     elsif ($type eq 'SOA') {
       fatal("$filename($.): duplicate SOA record: $fline")
